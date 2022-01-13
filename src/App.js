34b79e2edef1b335f297a0cd730f2c59165e7333
Tiffany <tiffanylouchao@gmail.com>
2022-01-13 03:14:42 -0500

    config webpack and babel loader

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+function App() {
+    return (<div>
+        <h1>Hello, Universe!</h1>
+        <h2>Millions of Galaxies!</h2>
+    </div>)
+}
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: "./src/index.js",
+  output: {
+  path: path.join(__dirname, '/build'),
+  filename: 'bundle.js'
+},
+devServer: {
+  static: {
+    directory: path.join(__dirname, 'public'),
+  },
+  port: 3000
+},
+module: {
+  rules: [
+    {
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader'
+      }
+    },
+    {
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader']
+    }
+  ]
+},
+plugins: [new HtmlWebpackPlugin({ template: './src/index.html' })],
+}
